25621a0642ada2a2de1f7753e2a5fff2e90b51c3
unknown <amincxo@gmail.com>
2024-10-12 03:21:36 +0330

    add error

--- src/components/HomePageWithAxios.jsx
+++ src/components/HomePageWithAxios.jsx
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query"
+import { isError, useQuery } from "@tanstack/react-query"
 import axios from "axios";
 
 function HomePageWithAxios() {
@@ -6,12 +6,17 @@ function HomePageWithAxios() {
     const fetchUsersAxios = () => axios.get("https://jsonplaceholder.typicode.com/users");
 
 
-    const {data , isLoading} = useQuery(["usersaxios"] ,fetchUsersAxios  );
+    const {data , isLoading , isError , error} = useQuery(["usersaxios"] ,fetchUsersAxios  );
 
-    console.log({data , isLoading});
+    console.log({data , isLoading , isError , error });
     if (isLoading) {
          return <h2>loadiiiiiiiing</h2>
     }
+
+    if (isError) {
+        return <h2> something went worng - {error.message} </h2>
+    }
+
   return (
     <>
         {data.data.map(i => 
