dfe0e009a5cf718a631e385c53d24dd3021bab1a
unknown <amincxo@gmail.com>
2024-10-12 18:54:58 +0330

    add use queryclient for invalid query and get query client to get twice posts

--- react-queryv5/src/App.jsx
+++ react-queryv5/src/App.jsx
@@ -1,21 +1,28 @@
-import {  useQuery  , useMutation} from "@tanstack/react-query"
+import {  useQuery  , useQueryClient} from "@tanstack/react-query"
 import { usePostMutation } from "./hooks/mutation"
 
 function App() {
 
-    // const queryKey = ["all-todos"];
-    // const queryFn = () => fetch("https://jsonplaceholder.typicode.com/todos").then((res) => res.json())
+    const queryClient = useQueryClient();
+
+    const queryKey = ["all-post"];
+    const queryFn = () => fetch("https://jsonplaceholder.typicode.com/post").then((res) => res.json())
    
-    //  const {isLoading ,data, isPending} =  useQuery({queryKey , queryFn})
-    // // is loading => isPanding && isFetching 
-    // console.log({isLoading, data , isPending})
+     const {isLoading ,data, isPending} =  useQuery({queryKey , queryFn})
+    // is loading => isPanding && isFetching 
+    console.log({isLoading, data , isPending})
 
 
-    const {mutate , data , isPending , }  = usePostMutation()
+    const {mutate }  = usePostMutation()
     
     const clickHandler = () => {
         const data = {name : "amin"}
-        mutate(data , {onSuccess: () => console.log("ersal") , onError: (error)=>console.log(error)  })
+        mutate(data , {onSuccess: () => {
+            console.log(data)
+            queryClient.invalidateQueries({
+                queryKey:["all-post"]
+            })
+        } , onError: (error)=>console.log(error)  })
     }
 
     console.log({data, isPending })
