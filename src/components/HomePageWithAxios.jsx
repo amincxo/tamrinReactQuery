af533d7d9746057c3738c7e66c27f2a96e2ac9dc
unknown <amincxo@gmail.com>
2024-10-12 12:05:53 +0330

    on sucsess and on error

--- src/components/HomePageWithAxios.jsx
+++ src/components/HomePageWithAxios.jsx
@@ -4,8 +4,10 @@ import { Link } from "react-router-dom";
 
 function HomePageWithAxios() {
 
-    const fetchUsersAxios = () => axios.get("https://jsonplaceholder.typicode.com/users");
+    const fetchUsersAxios = () => axios.get("https://json4placeholder.typicode.com/users");
+    const onSuccess = (data) => console.log("sucsess" , data?.data)
 
+    const onError = (error) => console.log("sucsess" , error)
 
     const {data , isLoading , isError , error ,isFetching , refetch } = useQuery(["usersaxios"] ,fetchUsersAxios ,{
         // cacheTime:50000,
@@ -13,7 +15,9 @@ function HomePageWithAxios() {
         // refetchOnMount:false,
         // refetchOnWindowFocus:false
         // refetchInterval: 2 * 1000
-        enabled:false
+        // enabled:false
+        onSuccess,
+        onError
     } );
 
     console.log({data , isLoading , isError , error , isFetching});
@@ -21,13 +25,12 @@ function HomePageWithAxios() {
     //      return <h2>loadiiiiiiiing</h2>
     // }
 
-    if (isError) {
-        return <h2> something went worng - {error.message} </h2>
-    }
+    // if (isError) {
+    //     return <h2> something went worng - {error.message} </h2>
+    // }
 
   return (
     <>
-        <button onClick={refetch} > گرفتن دیتا </button>
         <Link to="/q" > to axios </Link>
         {data?.data.map(i => 
         <h3 key={i.id} >
