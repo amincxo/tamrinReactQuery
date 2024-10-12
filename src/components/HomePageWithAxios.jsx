366a7a48785b9b5604d2fa6c4470875fc57cc1ec
unknown <amincxo@gmail.com>
2024-10-12 11:42:43 +0330

    stale time in react query

--- src/components/HomePageWithAxios.jsx
+++ src/components/HomePageWithAxios.jsx
@@ -9,7 +9,7 @@ function HomePageWithAxios() {
 
     const {data , isLoading , isError , error ,isFetching } = useQuery(["usersaxios"] ,fetchUsersAxios ,{
         // cacheTime:50000,
-        // staleTime:0
+        staleTime:120*1000,
     } );
 
     console.log({data , isLoading , isError , error , isFetching});
